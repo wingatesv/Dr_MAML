29ac4efb71979b9d94e6c920dd7c29d77bfdeca0
wingatesv <56868536+wingatesv@users.noreply.github.com>
2023-08-28 15:28:10 +0800

    Add files via upload

---
+++ filelists/BreaKHis_40x/get_BreakHis_40x.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -av /content/drive/MyDrive/PhD/BreaKHis_40x.zip /content/
+unzip /content/BreaKHis_40x.zip -d /content/BreaKHis_40x
---
+++ filelists/BreaKHis_40x/write_BreaKHis_40x_filelist.py
@@ -0,0 +1,54 @@
+import numpy as np
+from os import listdir
+from os.path import isfile, isdir, join
+import os
+import json
+import random
+
+
+
+data_path = '/content/BreaKHis_40x/BreaKHis_400x'
+savedir = '/content/drive/MyDrive/PhD/IDC_Grading_FSL/filelists/BreaKHis_40x/'
+dataset_list = ['base']
+
+folder_list = [f for f in listdir(data_path) if isdir(join(data_path, f))]
+folder_list.sort()
+label_dict = dict(zip(folder_list,range(0,len(folder_list))))
+
+classfile_list_all = []
+
+for i, folder in enumerate(folder_list):
+    folder_path = join(data_path, folder)
+    classfile_list_all.append( [ join(folder_path, cf) for cf in listdir(folder_path) if (isfile(join(folder_path,cf)) and cf[0] != '.')])
+    random.shuffle(classfile_list_all[i])
+
+
+for dataset in dataset_list:
+    file_list = []
+    label_list = []
+    for i, classfile_list in enumerate(classfile_list_all):
+        if 'base' in dataset:
+            file_list = file_list + classfile_list
+            label_list = label_list + np.repeat(i, len(classfile_list)).tolist()
+
+    fo = open(savedir + dataset + ".json", "w")
+    fo.write('{"label_names": [')
+    fo.writelines(['"%s",' % item  for item in folder_list])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write('],')
+
+    fo.write('"image_names": [')
+    fo.writelines(['"%s",' % item  for item in file_list])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write('],')
+
+    fo.write('"image_labels": [')
+    fo.writelines(['%d,' % item  for item in label_list])
+    fo.seek(0, os.SEEK_END) 
+    fo.seek(fo.tell()-1, os.SEEK_SET)
+    fo.write(']}')
+
+    fo.close()
+    print("%s -OK" %dataset)
