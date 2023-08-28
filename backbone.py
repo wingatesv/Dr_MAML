afda44a52a03e255fc499a85cceacb612473a2c3
wingatesv <56868536+wingatesv@users.noreply.github.com>
2023-08-28 22:04:35 +0800

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -7,8 +7,10 @@ import math
 import numpy as np
 import torch.nn.functional as F
 from torch.nn.utils.weight_norm import WeightNorm
+import torchvision.models as models
+import torch.nn.init as init
 
-# Basic ResNet model
+
 
 def init_layer(L):
     # Initialization using fan-in
@@ -284,44 +286,7 @@ class ConvNetNopool(nn.Module): #Relation net use a 4 layer conv with pooling in
         out = self.trunk(x)
         return out
 
-class ConvNetS(nn.Module): #For omniglot, only 1 input channel, output dim is 64
-    def __init__(self, depth, flatten = True):
-        super(ConvNetS,self).__init__()
-        trunk = []
-        for i in range(depth):
-            indim = 1 if i == 0 else 64
-            outdim = 64
-            B = ConvBlock(indim, outdim, pool = ( i <4 ) ) #only pooling for fist 4 layers
-            trunk.append(B)
 
-        if flatten:
-            trunk.append(Flatten())
-
-        self.trunk = nn.Sequential(*trunk)
-        self.final_feat_dim = 64
-
-    def forward(self,x):
-        out = x[:,0:1,:,:] #only use the first dimension
-        out = self.trunk(out)
-        return out
-
-class ConvNetSNopool(nn.Module): #Relation net use a 4 layer conv with pooling in only first two layers, else no pooling. For omniglot, only 1 input channel, output dim is [64,5,5]
-    def __init__(self, depth):
-        super(ConvNetSNopool,self).__init__()
-        trunk = []
-        for i in range(depth):
-            indim = 1 if i == 0 else 64
-            outdim = 64
-            B = ConvBlock(indim, outdim, pool = ( i in [0,1] ), padding = 0 if i in[0,1] else 1  ) #only first two layer has pooling and no padding
-            trunk.append(B)
-
-        self.trunk = nn.Sequential(*trunk)
-        self.final_feat_dim = [64,5,5]
-
-    def forward(self,x):
-        out = x[:,0:1,:,:] #only use the first dimension
-        out = self.trunk(out)
-        return out
 
 class ResNet(nn.Module):
     maml = False #Default
@@ -371,6 +336,351 @@ class ResNet(nn.Module):
         out = self.trunk(x)
         return out
 
+
+class ResNet_ImageNet(nn.Module):
+  def __init__(self, flatten = True, model = 'resnet18', frozen = True, layers=6):
+
+    super(ResNet_ImageNet, self).__init__()
+    self.name = model
+    self.flatten = flatten
+    last_layer = -1 if flatten else -2
+
+    if self.name =='resnet18':
+      self.weights = models.ResNet18_Weights.IMAGENET1K_V1
+      self.final_feat_dim = 512 if flatten else [512, 7, 7] #self.feature_extractor.fc.in_features
+    elif self.name == 'resnet34':
+      self.weights = models.ResNet34_Weights.IMAGENET1K_V1
+      self.final_feat_dim = 512 if flatten else [512, 7, 7]
+    elif self.name == 'resnet50':
+      self.weights = models.ResNet50_Weights.IMAGENET1K_V1
+      self.final_feat_dim = 2048 if flatten else [2048, 7, 7]
+    elif self.name == 'resnet101':
+      self.weights = models.ResNet101_Weights.IMAGENET1K_V1
+      self.final_feat_dim = 2048 if flatten else [2048, 7, 7]
+
+
+    self.feature_extractor = models.__dict__[self.name](weights=self.weights)
+
+    self.feature_extractor = nn.Sequential(*list(self.feature_extractor.children())[:last_layer])
+
+    #  set backbone training to False
+    if frozen:
+      print(f'Using Frozen ImageNet pre-trained weights, with last {layers} unfrozen')
+      for param in self.feature_extractor.parameters():
+        param.requires_grad = False
+      self.unfreeze_layers(num_layers = layers)
+      self.check_requires_grad()
+      
+    
+    # if frozen:
+    #   print('Freezing ImageNet pre-trained weights in Block 1 to 6 only, Block 7 (Last block) requires grad: True')
+    #   for name, module in self.feature_extractor.named_children():
+    #       if name != '7':
+    #         for param in module.parameters():
+    #           param.requires_grad = False
+    #   self.check_requires_grad()
+
+
+  def unfreeze_layers(self, num_layers):
+        total_layers = len(list(self.feature_extractor.parameters()))
+        layers_to_unfreeze = total_layers - num_layers
+        count = 0
+
+        for module in self.feature_extractor.children():
+            for param in module.parameters():
+                if count >= layers_to_unfreeze:
+                    param.requires_grad = True
+                count += 1
+
+
+  def forward(self, x):
+    x = self.feature_extractor(x)
+    if self.flatten:
+      x = nn.functional.adaptive_avg_pool2d(x, output_size=(1,1))
+      x = x.view(x.size(0), -1)
+    return x
+  
+  def check_requires_grad(self):
+    for name, param in self.named_parameters():
+      if param.requires_grad:
+         print(f'Layer: {name}\tRequires Grad: {param.requires_grad}')
+
+class SqueezeNet_ImageNet(nn.Module):
+    def __init__(self, flatten=True, frozen=True, layers=6):
+        super(SqueezeNet_ImageNet, self).__init__()
+        self.flatten = flatten
+
+        # Load pre-trained SqueezeNet model from ImageNet dataset
+        self.feature_extractor = models.squeezenet1_1(pretrained=True).features
+        # Freeze feature extractor layers if required
+        if frozen:
+            for param in self.feature_extractor.parameters():
+                param.requires_grad = False
+            self.unfreeze_layers(num_layers = layers)
+            self.check_requires_grad()
+      
+        
+        # Determine output feature dimension
+        if self.flatten:
+            self.final_feat_dim = 512
+        else:
+            self.final_feat_dim = [512, 13, 13]  # output size of last conv layer in SqueezeNet
+        
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        
+        if self.flatten:
+            x = nn.functional.adaptive_avg_pool2d(x, output_size=(1, 1))
+            x = x.view(x.size(0), -1)
+            
+        return x
+    
+    def unfreeze_layers(self, num_layers):
+          total_layers = len(list(self.feature_extractor.parameters()))
+          layers_to_unfreeze = total_layers - num_layers
+          count = 0
+
+          for module in self.feature_extractor.children():
+              for param in module.parameters():
+                  if count >= layers_to_unfreeze:
+                      param.requires_grad = True
+                  count += 1
+                  
+    def check_requires_grad(self):
+      for name, param in self.named_parameters():
+        print(f'Layer: {name}\tRequires Grad: {param.requires_grad}')
+
+
+class Fire(nn.Module):
+    def __init__(self, inplanes: int, squeeze_planes: int, expand1x1_planes: int, expand3x3_planes: int, maml=False) -> None:
+        super().__init__()
+        self.inplanes = inplanes
+        if maml:
+            self.squeeze = Conv2d_fw(inplanes, squeeze_planes, kernel_size=1)
+            self.expand1x1 = Conv2d_fw(squeeze_planes, expand1x1_planes, kernel_size=1)
+            self.expand3x3 = Conv2d_fw(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+            
+        else:
+            self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+            self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
+            self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+        
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.squeeze_activation(self.squeeze(x))
+        return torch.cat(
+            [self.expand1x1_activation(self.expand1x1(x)), self.expand3x3_activation(self.expand3x3(x))], 1
+        )
+
+class SqueezeNet(nn.Module):
+    maml = False
+    def __init__(self, flatten=True) -> None:
+        super().__init__()
+        self.flatten = flatten
+        self.final_feat_dim = 512 if flatten else [512,13,13]
+        if self.maml:
+            self.features = nn.Sequential(
+                    Conv2d_fw(3, 64, kernel_size=3, stride=2),
+                    nn.ReLU(inplace=True),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(64, 16, 64, 64, True),
+                    Fire(128, 16, 64, 64, True),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(128, 32, 128, 128, True),
+                    Fire(256, 32, 128, 128, True),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(256, 48, 192, 192, True),
+                    Fire(384, 48, 192, 192, True),
+                    Fire(384, 64, 256, 256, True),
+                    Fire(512, 64, 256, 256, True),
+                )
+            # Final convolution is initialized differently from the rest
+            final_conv = Conv2d_fw(512, 1000, kernel_size=1)
+        
+        else:
+            self.features = nn.Sequential(
+                    nn.Conv2d(3, 64, kernel_size=3, stride=2),
+                    nn.ReLU(inplace=True),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(64, 16, 64, 64),
+                    Fire(128, 16, 64, 64),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(128, 32, 128, 128),
+                    Fire(256, 32, 128, 128),
+                    nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                    Fire(256, 48, 192, 192),
+                    Fire(384, 48, 192, 192),
+                    Fire(384, 64, 256, 256),
+                    Fire(512, 64, 256, 256),
+                )
+            # Final convolution is initialized differently from the rest
+            final_conv = nn.Conv2d(512, 1000, kernel_size=1)
+
+
+        for m in self.modules():
+            if self.maml:
+                if isinstance(m, Conv2d_fw):
+                    if m is final_conv:
+                        init.normal_(m.weight, mean=0.0, std=0.01)
+                    else:
+                        init.kaiming_uniform_(m.weight)
+                    if m.bias is not None:
+                        init.constant_(m.bias, 0)
+            else:
+                if isinstance(m, nn.Conv2d):
+                    if m is final_conv:
+                        init.normal_(m.weight, mean=0.0, std=0.01)
+                    else:
+                        init.kaiming_uniform_(m.weight)
+                    if m.bias is not None:
+                        init.constant_(m.bias, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        if self.flatten:
+            x = nn.functional.adaptive_avg_pool2d(x, output_size=(1,1))
+            return torch.flatten(x, 1)
+        else:
+            return x
+
+class EffNet_ImageNet(nn.Module):
+    def __init__(self, flatten=True, frozen=True, layers=6):
+        super(EffNet_ImageNet, self).__init__()
+        self.flatten = flatten
+        self.frozen = frozen
+        self.feature_extractor = models.efficientnet_b0(weights = models.EfficientNet_B0_Weights.IMAGENET1K_V1).features
+
+        self.final_feat_dim = 1280 if flatten else [1280,7,7]
+
+
+        if self.frozen:
+          # Freeze the feature extractor weights
+          for param in self.feature_extractor.parameters():
+              param.requires_grad = False
+          self.unfreeze_layers(num_layers = layers)
+          self.check_requires_grad()
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        if self.flatten:
+            x = nn.functional.adaptive_avg_pool2d(x, output_size=(1,1))
+            x = x.view(x.size(0), -1)
+        return x
+
+
+    def unfreeze_layers(self, num_layers):
+          total_layers = len(list(self.feature_extractor.parameters()))
+          layers_to_unfreeze = total_layers - num_layers
+          count = 0
+
+          for module in self.feature_extractor.children():
+              for param in module.parameters():
+                  if count >= layers_to_unfreeze:
+                      param.requires_grad = True
+                  count += 1
+
+    def check_requires_grad(self):
+      for name, param in self.named_parameters():
+        if param.requires_grad:
+          print(f'Layer: {name}\tRequires Grad: {param.requires_grad}')
+
+class DenseNet_ImageNet(nn.Module):
+    def __init__(self, flatten=True, frozen=True, model='densenet201', layers=6):
+        super(DenseNet_ImageNet, self).__init__()
+        self.flatten = flatten
+        self.frozen = frozen
+
+        if model == 'densenet201':
+          self.feature_extractor = models.densenet201(weights = models.DenseNet201_Weights.IMAGENET1K_V1).features
+          self.final_feat_dim = 1920 if flatten else [1920,7,7]
+
+        elif model == 'densenet161':
+            self.feature_extractor = models.densenet161(weights = models.DenseNet161_Weights.IMAGENET1K_V1).features
+            self.final_feat_dim = 2208 if flatten else [2208,7,7]
+
+        if self.frozen:
+          # Freeze the feature extractor weights
+          for param in self.feature_extractor.parameters():
+              param.requires_grad = False
+          self.unfreeze_layers(num_layers = layers)
+          self.check_requires_grad()
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        if self.flatten:
+            x = nn.functional.adaptive_avg_pool2d(x, output_size=(1,1))
+            x = x.view(x.size(0), -1)
+        return x
+
+
+    def unfreeze_layers(self, num_layers):
+          total_layers = len(list(self.feature_extractor.parameters()))
+          layers_to_unfreeze = total_layers - num_layers
+          count = 0
+
+          for module in self.feature_extractor.children():
+              for param in module.parameters():
+                  if count >= layers_to_unfreeze:
+                      param.requires_grad = True
+                  count += 1
+
+    def check_requires_grad(self):
+      for name, param in self.named_parameters():
+        if param.requires_grad:
+          print(f'Layer: {name}\tRequires Grad: {param.requires_grad}')
+
+class MaxVit_ImageNet(nn.Module):
+    def __init__(self, flatten=True, frozen=True, layers=6):
+        super(MaxVit_ImageNet, self).__init__()
+        self.flatten = flatten
+        self.frozen = frozen
+        self.feature_extractor = models.maxvit_t(weights = models.MaxVit_T_Weights.IMAGENET1K_V1)
+        # Remove the classifier layer from the feature extractor
+        self.feature_extractor.classifier = nn.Identity()
+
+
+
+        self.final_feat_dim = 512 if flatten else [512,7,7]
+
+
+        if self.frozen:
+          # Freeze the feature extractor weights
+          for param in self.feature_extractor.parameters():
+              param.requires_grad = False
+          self.unfreeze_layers(num_layers = layers)
+          self.check_requires_grad()
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        if self.flatten:
+            x = nn.functional.adaptive_avg_pool2d(x, output_size=(1,1))
+            x = x.view(x.size(0), -1)
+        return x
+
+
+    def unfreeze_layers(self, num_layers):
+          total_layers = len(list(self.feature_extractor.parameters()))
+          layers_to_unfreeze = total_layers - num_layers
+          count = 0
+
+          for module in self.feature_extractor.children():
+              for param in module.parameters():
+                  if count >= layers_to_unfreeze:
+                      param.requires_grad = True
+                  count += 1
+
+    def check_requires_grad(self):
+      for name, param in self.named_parameters():
+         if param.requires_grad:
+           print(f'Layer: {name}\tRequires Grad: {param.requires_grad}')
+
+      
+
+
+
 def Conv4():
     return ConvNet(4)
 
@@ -383,27 +693,62 @@ def Conv4NP():
 def Conv6NP():
     return ConvNetNopool(6)
 
-def Conv4S():
-    return ConvNetS(4)
-
-def Conv4SNP():
-    return ConvNetSNopool(4)
 
 def ResNet10( flatten = True):
     return ResNet(SimpleBlock, [1,1,1,1],[64,128,256,512], flatten)
 
-def ResNet18( flatten = True):
-    return ResNet(SimpleBlock, [2,2,2,2],[64,128,256,512], flatten)
+def ResNet18( flatten = True, method=None):
+   if method == 'mammo':
+      return ResNet_ImageNet(flatten = True, model = 'resnet18', frozen=True, layers=6)
+   else:
+      return ResNet(SimpleBlock, [2,2,2,2],[64,128,256,512], flatten)
 
-def ResNet34( flatten = True):
-    return ResNet(SimpleBlock, [3,4,6,3],[64,128,256,512], flatten)
+def ResNet34( flatten = True, method=None):
+    if method == 'mammo':
+      return ResNet_ImageNet(flatten = True, model = 'resnet34', frozen=True, layers=6)
+    else:
+      return ResNet(SimpleBlock, [3,4,6,3],[64,128,256,512], flatten)
 
-def ResNet50( flatten = True):
-    return ResNet(BottleneckBlock, [3,4,6,3], [256,512,1024,2048], flatten)
+def ResNet50( flatten = True, method=None):
+    if method == 'mammo':
+        return ResNet_ImageNet(flatten = True, model = 'resnet50', frozen=True, layers=6)
+    else:
+        return ResNet(BottleneckBlock, [3,4,6,3], [256,512,1024,2048], flatten)
 
-def ResNet101( flatten = True):
-    return ResNet(BottleneckBlock, [3,4,23,3],[256,512,1024,2048], flatten)
+def ResNet101( flatten = True, method=None):
+    if method == 'mammo':
+          return ResNet_ImageNet(flatten = True, model = 'resnet101', frozen=True, layers=6)
+    else:
+          return ResNet(BottleneckBlock, [3,4,23,3],[256,512,1024,2048], flatten)
+
+def DenseNet201(flatten=True, frozen=True, model='densenet201', method=None):
+    if method == 'mammo':
+        return DenseNet_ImageNet(flatten=True, model=model, frozen=True, layers=6)
+    else:
+      raise AssertionError('Only MAMMO is supported')
 
 
+def DenseNet161(flatten=True, frozen=True, model='densenet161', method=None):
+    if method == 'mammo':
+        return DenseNet_ImageNet(flatten=True, model=model, frozen=True, layers=6)
+    else:
+      raise AssertionError('Only MAMMO is supported')
 
 
+def EffNet(flatten = True, frozen=True, method=None):
+  if method == 'mammo':
+      return EffNet_ImageNet(flatten=True, frozen=True, layers=6)
+  else:
+      raise AssertionError('Only MAMMO is supported')
+
+def MaxVit(flatten = True, frozen=True, method=None):
+  if method == 'mammo':
+      return MaxVit_ImageNet(flatten=True, frozen=True, layers=6)
+  else:
+    raise AssertionError('Only MAMMO is supported')
+
+def squeezenet(flatten=True, frozen=True, method=None):
+  if method == 'mammo':
+    return SqueezeNet_ImageNet(flatten=True, frozen=True, layers=6)
+  else:
+    return SqueezeNet(flatten)
