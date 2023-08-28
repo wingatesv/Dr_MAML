6feb7eba761b681a3af0da52654d52a7dedf281c
wingatesv <56868536+wingatesv@users.noreply.github.com>
2023-08-28 22:05:07 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,12 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+ROOT_DIR = '/content/drive/MyDrive/PhD/IDC_Grading_FSL'
+
+save_dir = ROOT_DIR + '/best_model'
 data_dir = {}
-data_dir['CUB']             = './filelists/CUB/' 
-data_dir['miniImagenet']    = './filelists/miniImagenet/' 
-data_dir['omniglot']        = './filelists/omniglot/' 
-data_dir['emnist']          = './filelists/emnist/' 
+data_dir['BreaKHis_4x']  = ROOT_DIR + '/filelists/BreaKHis_4x/' 
+data_dir['BreaKHis_10x']  = ROOT_DIR + '/filelists/BreaKHis_10x/' 
+data_dir['BreaKHis_20x']  = ROOT_DIR + '/filelists/BreaKHis_20x/' 
+data_dir['BreaKHis_40x']  = ROOT_DIR + '/filelists/BreaKHis_40x/' 
+data_dir['BCHI'] = ROOT_DIR + '/filelists/BCHI/' 
+data_dir['PathoIDC_20x'] = ROOT_DIR + '/filelists/PathoIDC_20x/' 
+data_dir['PathoIDC_40x'] = ROOT_DIR + '/filelists/PathoIDC_40x/' 
+data_dir['Databiox'] = ROOT_DIR + '/filelists/Databiox/' 
