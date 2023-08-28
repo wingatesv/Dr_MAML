a9478f3dad745dd292cf97269684b988c4990c80
wingatesv <56868536+wingatesv@users.noreply.github.com>
2023-08-28 22:09:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,8 +7,13 @@ import torch.optim.lr_scheduler as lr_scheduler
 import time
 import os
 import glob
+import math
+import collections
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 import configs
+
 import backbone
 from data.datamgr import SimpleDataManager, SetDataManager
 from methods.baselinetrain import BaselineTrain
@@ -17,147 +22,241 @@ from methods.protonet import ProtoNet
 from methods.matchingnet import MatchingNet
 from methods.relationnet import RelationNet
 from methods.maml import MAML
+from methods.anil import ANIL
+from methods.mammo import MAMMO
+from methods.sharpmaml import SharpMAML
+from sam import SAM
+
+
+import torch.multiprocessing as mp
 from io_utils import model_dict, parse_args, get_resume_file  
 
+
+
 def train(base_loader, val_loader, model, optimization, start_epoch, stop_epoch, params):    
+
+   
+
     if optimization == 'Adam':
-        optimizer = torch.optim.Adam(model.parameters())
+        # sharpness aware minimisation
+        if params.method  == 'sharpmaml':
+          print('Using SAM Optimizer.....')
+          base_optimizer = torch.optim.Adam 
+          optimizer = SAM(model.parameters(), base_optimizer, lr=0.001)
+
+        else:
+          if hasattr(model, 'task_lr'):
+              learning_rate = 0.00001
+              print(f'With Adaptive Learnable Learning rate, Adam LR:{learning_rate}')
+              model.define_task_lr_params()
+              model_params = list(model.parameters()) + list(model.task_lr.values())
+              optimizer = torch.optim.Adam(model_params, lr=learning_rate)
+          else:
+              learning_rate = 0.0001
+              print(f'With scalar Learning rate, Adam LR:{learning_rate}')
+              optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+
+
     else:
        raise ValueError('Unknown optimization, please define by yourself')
+  
+    max_acc = 0   
+    total_training_time = 0
+    val_acc = []    
+    scheduler = None
+    # scheduler = lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=(stop_epoch-start_epoch), eta_min=0.0001)
 
-    max_acc = 0       
 
     for epoch in range(start_epoch,stop_epoch):
+        start_time = time.time() # record start time
         model.train()
-        model.train_loop(epoch, base_loader,  optimizer ) #model are called by reference, no need to return 
+        model.train_loop(epoch, base_loader,  optimizer) #model are called by reference, no need to return 
         model.eval()
 
         if not os.path.isdir(params.checkpoint_dir):
             os.makedirs(params.checkpoint_dir)
 
-        acc = model.test_loop( val_loader)
+        acc = model.test_loop(val_loader)
+        val_acc.append(acc)
         if acc > max_acc : #for baseline and baseline++, we don't use validation in default and we let acc = -1, but we allow options to validate with DB index
             print("best model! save...")
             max_acc = acc
             outfile = os.path.join(params.checkpoint_dir, 'best_model.tar')
-            torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
+            if hasattr(model, 'task_lr'):
+                torch.save({'epoch':epoch, 'state':model.state_dict(), 'task_lr': model.task_lr}, outfile)
+            else:
+                torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
+
 
         if (epoch % params.save_freq==0) or (epoch==stop_epoch-1):
             outfile = os.path.join(params.checkpoint_dir, '{:d}.tar'.format(epoch))
-            torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
+            if hasattr(model, 'task_lr'):
+                torch.save({'epoch':epoch, 'state':model.state_dict(), 'task_lr': model.task_lr}, outfile)
+            else:
+                torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
 
+            
+        elapsed_time = time.time() - start_time # calculate elapsed time
+        total_training_time += elapsed_time
+      
+        
+    elapsed_hours = total_training_time / 3600.0 # convert to hours
+    print(f"Total Training Time: {elapsed_hours:.2f} h") # print elapsed time for current epoch in hours
+
+
+    plt.plot(range(start_epoch, stop_epoch), val_acc)
+    plt.xlabel('Epoch')
+    plt.ylabel('Validation Accuracy')
+    plt.savefig(f'{params.checkpoint_dir}_val_acc.png')
     return model
 
 if __name__=='__main__':
+    mp.set_start_method('spawn')
     np.random.seed(10)
     params = parse_args('train')
 
 
-    if params.dataset == 'cross':
-        base_file = configs.data_dir['miniImagenet'] + 'all.json' 
-        val_file   = configs.data_dir['CUB'] + 'val.json' 
-    elif params.dataset == 'cross_char':
-        base_file = configs.data_dir['omniglot'] + 'noLatin.json' 
-        val_file   = configs.data_dir['emnist'] + 'val.json' 
+    if params.dataset == 'cross_IDC_4x':
+        base_file = configs.data_dir['BreaKHis_4x'] + 'base.json' 
+        val_file   = configs.data_dir['BCHI'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_10x':
+        base_file = configs.data_dir['BreaKHis_10x'] + 'base.json' 
+        val_file   = configs.data_dir['BCHI'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_20x':
+        base_file = configs.data_dir['BreaKHis_20x'] + 'base.json' 
+        val_file   = configs.data_dir['BCHI'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_40x':
+        base_file = configs.data_dir['BreaKHis_40x'] + 'base.json' 
+        val_file   = configs.data_dir['BCHI'] + 'val.json' 
+
+
+    elif params.dataset == 'cross_IDC_4x_2':
+        base_file = configs.data_dir['BreaKHis_4x'] + 'base.json' 
+        val_file   = configs.data_dir['PathoIDC_40x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_10x_2':
+            base_file = configs.data_dir['BreaKHis_10x'] + 'base.json' 
+            val_file   = configs.data_dir['PathoIDC_40x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_20x_2':
+        base_file = configs.data_dir['BreaKHis_20x'] + 'base.json' 
+        val_file   = configs.data_dir['PathoIDC_40x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_40x_2':
+            base_file = configs.data_dir['BreaKHis_40x'] + 'base.json' 
+            val_file   = configs.data_dir['PathoIDC_40x'] + 'val.json' 
+
+    elif params.dataset == 'cross_IDC_4x_3':
+        base_file = configs.data_dir['BreaKHis_4x'] + 'base.json' 
+        val_file   = configs.data_dir['PathoIDC_20x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_10x_3':
+            base_file = configs.data_dir['BreaKHis_10x'] + 'base.json' 
+            val_file   = configs.data_dir['PathoIDC_20x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_20x_3':
+        base_file = configs.data_dir['BreaKHis_20x'] + 'base.json' 
+        val_file   = configs.data_dir['PathoIDC_20x'] + 'val.json' 
+    elif params.dataset == 'cross_IDC_40x_3':
+            base_file = configs.data_dir['BreaKHis_40x'] + 'base.json' 
+            val_file   = configs.data_dir['PathoIDC_20x'] + 'val.json' 
+
+
+    elif params.dataset == 'cross_IDC_3':
+        base_file = configs.data_dir['BreaKHis'] + 'base.json' 
+        val_file   = configs.data_dir['Databiox'] + 'val.json' 
     else:
         base_file = configs.data_dir[params.dataset] + 'base.json' 
         val_file   = configs.data_dir[params.dataset] + 'val.json' 
          
     if 'Conv' in params.model:
-        if params.dataset in ['omniglot', 'cross_char']:
-            image_size = 28
-        else:
-            image_size = 84
+      image_size = 84
+    elif 'EffNet' in params.model:
+      image_size = 480
     else:
-        image_size = 224
+      image_size = 224
 
-    if params.dataset in ['omniglot', 'cross_char']:
-        assert params.model == 'Conv4' and not params.train_aug ,'omniglot only support Conv4 without augmentation'
-        params.model = 'Conv4S'
 
     optimization = 'Adam'
 
     if params.stop_epoch == -1: 
         if params.method in ['baseline', 'baseline++'] :
-            if params.dataset in ['omniglot', 'cross_char']:
-                params.stop_epoch = 5
-            elif params.dataset in ['CUB']:
-                params.stop_epoch = 200 # This is different as stated in the open-review paper. However, using 400 epoch in baseline actually lead to over-fitting
-            elif params.dataset in ['miniImagenet', 'cross']:
-                params.stop_epoch = 400
-            else:
-                params.stop_epoch = 400 #default
-        else: #meta-learning methods
-            if params.n_shot == 1:
-                params.stop_epoch = 600
-            elif params.n_shot == 5:
-                params.stop_epoch = 400
-            else:
-                params.stop_epoch = 600 #default
+            params.stop_epoch = 50
      
+        else: # other meta-learning methods
+         params.stop_epoch = 100 
 
+    print(f'Applying {params.sn} stain normalisation......') if params.sn else print()
+    
     if params.method in ['baseline', 'baseline++'] :
-        base_datamgr    = SimpleDataManager(image_size, batch_size = 16)
-        base_loader     = base_datamgr.get_data_loader( base_file , aug = params.train_aug )
-        val_datamgr     = SimpleDataManager(image_size, batch_size = 64)
-        val_loader      = val_datamgr.get_data_loader( val_file, aug = False)
-        
-        if params.dataset == 'omniglot':
-            assert params.num_classes >= 4112, 'class number need to be larger than max label id in base class'
-        if params.dataset == 'cross_char':
-            assert params.num_classes >= 1597, 'class number need to be larger than max label id in base class'
-
-        if params.method == 'baseline':
+      base_datamgr    = SimpleDataManager(image_size, batch_size = 16)
+      base_loader     = base_datamgr.get_data_loader( base_file , aug = params.train_aug, sn = params.sn)
+      val_datamgr     = SimpleDataManager(image_size, batch_size = 64)
+      val_loader      = val_datamgr.get_data_loader( val_file, aug = False, sn = params.sn)
+     
+      if params.method == 'baseline':
             model           = BaselineTrain( model_dict[params.model], params.num_classes)
-        elif params.method == 'baseline++':
+      elif params.method == 'baseline++':
             model           = BaselineTrain( model_dict[params.model], params.num_classes, loss_type = 'dist')
 
-    elif params.method in ['protonet','matchingnet','relationnet', 'relationnet_softmax', 'maml', 'maml_approx']:
+    elif params.method in ['protonet','matchingnet','relationnet', 'relationnet_softmax', 'maml', 'maml_approx', 'anil', 'mammo', 'sharpmaml']:
+       
         n_query = max(1, int(16* params.test_n_way/params.train_n_way)) #if test_n_way is smaller than train_n_way, reduce n_query to keep batch size small
- 
+
         train_few_shot_params    = dict(n_way = params.train_n_way, n_support = params.n_shot) 
-        base_datamgr            = SetDataManager(image_size, n_query = n_query,  **train_few_shot_params)
-        base_loader             = base_datamgr.get_data_loader( base_file , aug = params.train_aug )
-         
         test_few_shot_params     = dict(n_way = params.test_n_way, n_support = params.n_shot) 
-        val_datamgr             = SetDataManager(image_size, n_query = n_query, **test_few_shot_params)
-        val_loader              = val_datamgr.get_data_loader( val_file, aug = False) 
-        #a batch for SetDataManager: a [n_way, n_support + n_query, dim, w, h] tensor        
+
+        base_datamgr = SetDataManager(image_size, n_query = n_query,  **train_few_shot_params)
+        base_loader  = base_datamgr.get_data_loader( base_file , aug = params.train_aug,  sn = params.sn)
+        
+        val_datamgr = SetDataManager(image_size, n_query = n_query, **test_few_shot_params)
+        val_loader = val_datamgr.get_data_loader( val_file, aug = False, sn = params.sn) 
+        #a batch for SetDataManager: a [n_way, n_support + n_query, dim, w, h] tensor  
 
         if params.method == 'protonet':
-            model           = ProtoNet( model_dict[params.model], **train_few_shot_params )
+            model = ProtoNet( model_dict[params.model], **train_few_shot_params )
         elif params.method == 'matchingnet':
-            model           = MatchingNet( model_dict[params.model], **train_few_shot_params )
+            model = MatchingNet( model_dict[params.model], **train_few_shot_params )
         elif params.method in ['relationnet', 'relationnet_softmax']:
             if params.model == 'Conv4': 
                 feature_model = backbone.Conv4NP
             elif params.model == 'Conv6': 
                 feature_model = backbone.Conv6NP
-            elif params.model == 'Conv4S': 
-                feature_model = backbone.Conv4SNP
             else:
                 feature_model = lambda: model_dict[params.model]( flatten = False )
             loss_type = 'mse' if params.method == 'relationnet' else 'softmax'
 
-            model           = RelationNet( feature_model, loss_type = loss_type , **train_few_shot_params )
-        elif params.method in ['maml' , 'maml_approx']:
-            backbone.ConvBlock.maml = True
-            backbone.SimpleBlock.maml = True
-            backbone.BottleneckBlock.maml = True
-            backbone.ResNet.maml = True
-            model           = MAML(  model_dict[params.model], approx = (params.method == 'maml_approx') , **train_few_shot_params )
-            if params.dataset in ['omniglot', 'cross_char']: #maml use different parameter in omniglot
-                model.n_task     = 32
-                model.task_update_num = 1
-                model.train_lr = 0.1
-    else:
-       raise ValueError('Unknown method')
+            model = RelationNet( feature_model, loss_type = loss_type , **train_few_shot_params )
+
+
+        elif params.method in ['maml' , 'maml_approx', 'anil', 'mammo', 'sharpmaml']:
+          backbone.ConvBlock.maml = True
+          backbone.SimpleBlock.maml = True
+          backbone.BottleneckBlock.maml = True
+          backbone.ResNet.maml = True
+          backbone.SqueezeNet.maml = True
+
+          if params.method in ['maml', 'maml_approx']:
+            model = MAML(  model_dict[params.model], approx = (params.method == 'maml_approx') , **train_few_shot_params )
+       
+          elif params.method == 'anil':
+            model = ANIL(  model_dict[params.model], approx = False , **train_few_shot_params )
+
+          elif params.method == 'mammo':
+            assert params.model not in ['Conv4', 'Conv6','Conv4NP', 'Conv6NP', 'ResNet10'], 'mammo do not support non-ImageNet pretrained model'
+            feature_backbone = lambda: model_dict[params.model]( flatten = True, method = params.method)
+            model = MAMMO(  feature_backbone, approx = False , **train_few_shot_params )
+            # model = MAMMO(  model_dict[params.model], approx = False , **train_few_shot_params )
+
+          elif params.method == 'sharpmaml':
+            model = SharpMAML(  model_dict[params.model], approx = False , **train_few_shot_params )
+              
+        else:
+          raise ValueError('Unknown method')
 
     model = model.cuda()
 
     params.checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(configs.save_dir, params.dataset, params.model, params.method)
     if params.train_aug:
         params.checkpoint_dir += '_aug'
+    if params.sn == 'stainnet':
+        params.checkpoint_dir += '_stainnet'
     if not params.method  in ['baseline', 'baseline++']: 
         params.checkpoint_dir += '_%dway_%dshot' %( params.train_n_way, params.n_shot)
 
@@ -166,8 +265,7 @@ if __name__=='__main__':
 
     start_epoch = params.start_epoch
     stop_epoch = params.stop_epoch
-    if params.method == 'maml' or params.method == 'maml_approx' :
-        stop_epoch = params.stop_epoch * model.n_task #maml use multiple tasks in one update 
+   
 
     if params.resume:
         resume_file = get_resume_file(params.checkpoint_dir)
@@ -175,23 +273,6 @@ if __name__=='__main__':
             tmp = torch.load(resume_file)
             start_epoch = tmp['epoch']+1
             model.load_state_dict(tmp['state'])
-    elif params.warmup: #We also support warmup from pretrained baseline feature, but we never used in our paper
-        baseline_checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(configs.save_dir, params.dataset, params.model, 'baseline')
-        if params.train_aug:
-            baseline_checkpoint_dir += '_aug'
-        warmup_resume_file = get_resume_file(baseline_checkpoint_dir)
-        tmp = torch.load(warmup_resume_file)
-        if tmp is not None: 
-            state = tmp['state']
-            state_keys = list(state.keys())
-            for i, key in enumerate(state_keys):
-                if "feature." in key:
-                    newkey = key.replace("feature.","")  # an architecture model has attribute 'feature', load architecture feature to backbone by casting name from 'feature.trunk.xx' to 'trunk.xx'  
-                    state[newkey] = state.pop(key)
-                else:
-                    state.pop(key)
-            model.feature.load_state_dict(state)
-        else:
-            raise ValueError('No warm_up file')
+
 
     model = train(base_loader, val_loader,  model, optimization, start_epoch, stop_epoch, params)
