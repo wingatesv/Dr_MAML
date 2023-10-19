4b101c74812448ffd2ea36712b704e6fd5f5ad3d
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2023-10-19 11:31:20 +0800

    Update io_utils.py

--- io_utils.py
+++ io_utils.py
@@ -24,7 +24,7 @@ def parse_args(script):
     parser = argparse.ArgumentParser(description= 'few-shot script %s' %(script))
     parser.add_argument('--dataset'     , default='cross_IDC_40x',        help='cross_IDC_4x,cross_IDC_10x,cross_IDC_20x,cross_IDC_40x, cross_IDC_4x_2,cross_IDC_10x_2,cross_IDC_20x_2,cross_IDC_40x_2')
     parser.add_argument('--model'       , default='ResNet10',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}') # 50 and 101 are not used in the paper
-    parser.add_argument('--method'      , default='baseline',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/mammo') #relationnet_softmax replace L2 norm with softmax to expedite training, maml_approx use first-order approximation in the gradient for efficiency
+    parser.add_argument('--method'      , default='baseline',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/imaml_idcg') #relationnet_softmax replace L2 norm with softmax to expedite training, maml_approx use first-order approximation in the gradient for efficiency
     parser.add_argument('--train_n_way' , default=3, type=int,  help='class num to classify for training') #baseline and baseline++ would ignore this parameter
     parser.add_argument('--test_n_way'  , default=3, type=int,  help='class num to classify for testing (validation) ') #baseline and baseline++ only use this parameter in finetuning
     parser.add_argument('--n_shot'      , default=1, type=int,  help='number of labeled data in each class, same as n_support') #baseline and baseline++ only use this parameter in finetuning
@@ -49,8 +49,8 @@ def parse_args(script):
         parser.add_argument('--adaptation'  , action='store_true', help='further adaptation in test time or not')
 
     elif script == 'grad_cam':
-        parser.add_argument('--method_1'      , default='maml',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/mammo') 
-        parser.add_argument('--method_2'      , default='anil',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/mammo') 
+        parser.add_argument('--method_1'      , default='maml',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/imaml_idcg') 
+        parser.add_argument('--method_2'      , default='anil',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/imaml_idcg') 
         parser.add_argument('--model_1'       , default='ResNet34',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}')
         parser.add_argument('--model_2'       , default='ResNet34_IM_F',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}')
         parser.add_argument('--split'       , default='novel', help='base/val/novel') #default novel, but you can also test base/val class accuracy if you want 
