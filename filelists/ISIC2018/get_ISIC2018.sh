185c6d83ad3481febab6d001fb213ad46d0ed3eb
Wingates Voon <wingatesv@gmail.com>
2023-10-24 10:29:38 +0000

    Update the main branch

--- filelists/ISIC2018/get_ISIC2018.sh
+++ filelists/ISIC2018/get_ISIC2018.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ..
 wget https://isic-challenge-data.s3.amazonaws.com/2018/ISIC2018_Task3_Training_Input.zip
 unzip /content/ISIC2018_Task3_Training_Input.zip 
+python /content/Dr_MAML/filelists/ISIC2018/prepare_data.py
