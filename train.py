a31f55e12c44c5922004725833c5897866285488
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-03-10 10:28:54 +0800

    Update train.py

--- train.py
+++ train.py
@@ -257,8 +257,8 @@ if __name__=='__main__':
                 raise ValueError('Unknown Annealing Parameters')
             model = LRANNEMAML(  model_dict[params.model], 
                              annealing_type = str(anneal_params[0]), 
-                             initial_inner_lr = int(anneal_params[1]), 
-                             final_inner_lr = int(anneal_params[2]), 
+                             initial_inner_lr = float(anneal_params[1]), 
+                             final_inner_lr = float(anneal_params[2]), 
                              annealing_rate = float(anneal_params[3]),
                              test_mode = False,
                              approx = False, 
