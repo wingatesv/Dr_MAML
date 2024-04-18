18065b6de575fd60a586e4eaf96ed102b7e012c0
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-04-19 00:20:29 +0800

    Update anneal_maml.py

--- methods/anneal_maml.py
+++ methods/anneal_maml.py
@@ -11,6 +11,43 @@ from tqdm import tqdm
 import math
 import random
 
+def new_trapezoidal_step_scheduler(total_epochs, current_epoch, max_step, min_step, max_step_width):
+    # Calculate the midpoint of total_epochs and max_step_width
+    epoch_midpoint = total_epochs // 2
+    max_step_width_midpoint = max_step_width / 2
+
+    # Calculate the frequency of half max step and the starting and ending epochs of max step frequency
+    half_max_step_freq = math.floor(total_epochs * max_step_width_midpoint)
+    max_step_freq_start_epoch = epoch_midpoint - half_max_step_freq
+    max_step_freq_end_epoch = epoch_midpoint + half_max_step_freq
+
+    # Calculate the size of max step and total step frequency
+    max_step_size = max_step_freq_end_epoch - max_step_freq_start_epoch
+    total_step_freq_size = total_epochs - max_step_size
+
+    # Calculate the size of half step frequency and the frequency of step
+    half_step_freq_size = total_step_freq_size // 2
+    step_freq = math.floor(half_step_freq_size / (max_step - 1))
+
+    # Calculate the total step frequency and adjust max step size if necessary
+    total_step_freq = (max_step - 1) * step_freq * 2
+    if total_step_freq < (total_epochs - max_step_size):
+        step_diff = total_epochs - total_step_freq - max_step_size
+        max_step_size += step_diff
+
+    # Calculate the new half step frequency size
+    new_half_step_freq_size = total_step_freq // 2
+
+    # Determine the current step based on the current epoch
+    if current_epoch < new_half_step_freq_size:
+        current_step = min_step + current_epoch // step_freq
+    elif current_epoch < (new_half_step_freq_size + max_step_size):
+        current_step = max_step
+    else:
+        current_step = (max_step - 1) - (current_epoch - step_freq * (max_step - 1) - max_step_size) // step_freq
+
+    return current_step
+
 def trapezoidal_step_scheduler(total_epochs, current_epoch, max_adaptation_step, min_adaptation_step,  include_max_phase=True, half=False):
     # Guardrails to ensure correct types and values for the parameters
     assert isinstance(total_epochs, int), "total_epochs must be an integer"
@@ -152,6 +189,9 @@ class ANNEMAML(MetaTemplate):
 
       elif atype == 'up_tra_2':
           return trapezoidal_step_scheduler(total_epochs = epochs, current_epoch = current_epoch, max_adaptation_step = task_update_num_initial, min_adaptation_step = task_update_num_final,  include_max_phase=True, half=True)
+
+      elif atype == 'tra_3':
+          return trapezoidal_step_scheduler(total_epochs = epochs, current_epoch = current_epoch, max_step = task_update_num_initial, min_step = task_update_num_final, max_step_width = annealing_rate)
 
     def set_epoch(self, epoch):
         self.current_epoch = epoch
