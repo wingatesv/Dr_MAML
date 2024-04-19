2c924797268316f4ebf6811c143f74ba24bc2094
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-04-19 15:18:50 +0800

    Update anneal_maml.py

--- methods/anneal_maml.py
+++ methods/anneal_maml.py
@@ -12,6 +12,7 @@ import math
 import random
 
 def new_trapezoidal_step_scheduler(total_epochs, current_epoch, max_step, min_step, max_step_width):
+    step_diff = 0
     # Calculate the midpoint of total_epochs and max_step_width
     epoch_midpoint = total_epochs // 2
     max_step_width_midpoint = max_step_width / 2
@@ -33,18 +34,27 @@ def new_trapezoidal_step_scheduler(total_epochs, current_epoch, max_step, min_st
     total_step_freq = (max_step - 1) * step_freq * 2
     if total_step_freq < (total_epochs - max_step_size):
         step_diff = total_epochs - total_step_freq - max_step_size
-        max_step_size += step_diff
+        max_step_size = max_step_size + step_diff if max_step_width != 0 else max_step_size
 
     # Calculate the new half step frequency size
     new_half_step_freq_size = total_step_freq // 2
 
-    # Determine the current step based on the current epoch
-    if current_epoch < new_half_step_freq_size:
-        current_step = min_step + current_epoch // step_freq
-    elif current_epoch < (new_half_step_freq_size + max_step_size):
-        current_step = max_step
+    if max_step_width == 0 and step_diff > 0:
+         # Determine the current step based on the current epoch
+        if current_epoch < new_half_step_freq_size:
+            current_step = min_step + current_epoch // step_freq
+        elif current_epoch < (new_half_step_freq_size + step_diff):
+            current_step = (max_step - 1)
+        else:
+            current_step = (max_step - 1) - (current_epoch - step_freq * (max_step - 1) - step_diff) // step_freq       
     else:
-        current_step = (max_step - 1) - (current_epoch - step_freq * (max_step - 1) - max_step_size) // step_freq
+        # Determine the current step based on the current epoch
+        if current_epoch < new_half_step_freq_size:
+            current_step = min_step + current_epoch // step_freq
+        elif current_epoch < (new_half_step_freq_size + max_step_size):
+            current_step = max_step
+        else:
+            current_step = (max_step - 1) - (current_epoch - step_freq * (max_step - 1) - max_step_size) // step_freq
 
     return current_step
 
