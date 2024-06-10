0901dc4ef22a3f775a450da2f7add23642e7e5c1
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-06-10 14:11:52 +0800

    Create ppo_maml.py

---
+++ methods/ppo_maml.py
@@ -0,0 +1,113 @@
+class MAML(MetaTemplate):
+    def __init__(self, model_func, n_way, n_support, approx=False):
+        super(MAML, self).__init__(model_func, n_way, n_support, change_way=False)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.classifier = backbone.Linear_fw(self.feat_dim, n_way)
+        self.classifier.bias.data.fill_(0)
+        
+        self.n_task = 4
+        self.task_update_num = 5
+        self.train_lr = 0.01
+        self.approx = approx
+
+        self.inner_loop_steps_list = []
+
+        # Placeholder for train and val losses
+        self.current_train_loss = None
+        self.current_val_loss = None
+
+    def forward(self, x):
+        out = self.feature.forward(x)
+        scores = self.classifier.forward(out)
+        return scores
+
+    def set_forward(self, x, is_feature=False):
+        assert not is_feature, 'MAML does not support fixed feature'
+        x = x.cuda()
+        x_var = Variable(x)
+        x_a_i = x_var[:, :self.n_support, :, :, :].contiguous().view(self.n_way * self.n_support, *x.size()[2:]) 
+        x_b_i = x_var[:, self.n_support:, :, :, :].contiguous().view(self.n_way * self.n_query, *x.size()[2:]) 
+        y_a_i = Variable(torch.from_numpy(np.repeat(range(self.n_way), self.n_support))).cuda()
+        
+        fast_parameters = list(self.parameters())
+        for weight in self.parameters():
+            weight.fast = None
+        self.zero_grad()
+
+        for task_step in range(self.task_update_num):
+            scores = self.forward(x_a_i)
+            set_loss = self.loss_fn(scores, y_a_i)
+            grad = torch.autograd.grad(set_loss, fast_parameters, create_graph=True)
+            if self.approx:
+                grad = [g.detach() for g in grad]
+            fast_parameters = []
+            for k, weight in enumerate(self.parameters()):
+                if weight.fast is None:
+                    weight.fast = weight - self.train_lr * grad[k]
+                else:
+                    weight.fast = weight.fast - self.train_lr * grad[k]
+                fast_parameters.append(weight.fast)
+
+        scores = self.forward(x_b_i)
+        return scores
+
+    def set_forward_loss(self, x):
+        scores = self.set_forward(x, is_feature=False)
+        y_b_i = Variable(torch.from_numpy(np.repeat(range(self.n_way), self.n_query))).cuda()
+        loss = self.loss_fn(scores, y_b_i)
+        return loss
+
+    def train_loop(self, epoch, train_loader, optimizer, ppo_model):
+        print_freq = 10
+        avg_loss = 0
+        task_count = 0
+        loss_all = []
+
+        optimizer.zero_grad()
+        for i, (x, _) in enumerate(train_loader):
+            self.n_query = x.size(1) - self.n_support
+            assert self.n_way == x.size(0), "MAML does not support way change"
+
+            obs = np.array([self.current_train_loss, self.current_val_loss])
+            action, _ = ppo_model.predict(obs)
+            self.task_update_num = action + 1
+
+            loss = self.set_forward_loss(x)
+            avg_loss += loss.item()
+            loss_all.append(loss)
+
+            task_count += 1
+            if task_count == self.n_task:
+                loss_q = torch.stack(loss_all).sum(0)
+                loss_value = loss_q.item()
+                loss_q.backward()
+                optimizer.step()
+
+                task_count = 0
+                loss_all = []
+            optimizer.zero_grad()
+            if i % print_freq == 0:
+                print(f'Epoch {epoch} | Batch {i}/{len(train_loader)} | Loss {avg_loss / float(i + 1):.6f}')
+
+    def test_loop(self, test_loader, return_std=False):
+        correct = 0
+        count = 0
+        avg_loss = 0
+        acc_all = []
+
+        iter_num = len(test_loader)
+        for i, (x, _) in enumerate(tqdm(test_loader, desc='Testing', leave=False)):
+            self.n_query = x.size(1) - self.n_support
+            assert self.n_way == x.size(0), "MAML does not support way change"
+            correct_this, count_this, loss = self.correct(x)
+            acc_all.append(correct_this / count_this * 100)
+            avg_loss += loss.item()
+
+        acc_all = np.asarray(acc_all)
+        acc_mean = np.mean(acc_all)
+        acc_std = np.std(acc_all)
+        print(f'{iter_num} Test Acc = {acc_mean:.2f}% ± {1.96 * acc_std / np.sqrt(iter_num):.2f}%, Test Loss = {avg_loss / iter_num:.4f}')
+        if return_std:
+            return acc_mean, acc_std, float(avg_loss / iter_num)
+        else:
+            return acc_mean, float(avg_loss / iter_num)
