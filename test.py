b5e3304e7a456f541acded0c94813393a62a6c95
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-06-16 20:15:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         model = ANIL(  model_dict[params.model], approx = False , **few_shot_params )
 
       elif params.method == 'ppo_maml':
-        model = PPO_MAML(  model_dict[params.model], approx = False, env  = MAMLEnv(), **few_shot_params )
+        model = PPO_MAML(  model_dict[params.model], approx = False, env  = MAMLEnv(), test_mode = True,  **few_shot_params )
 
       elif params.method == 'annemaml':     
         if params.anneal_param != 'none':
