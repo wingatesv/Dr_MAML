adf48050497dab9fa47e6588c8b1633d90eb1975
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-06-21 12:07:44 +0800

    Update PPO_MAML_2.py

--- methods/PPO_MAML_2.py
+++ methods/PPO_MAML_2.py
@@ -1,5 +1,6 @@
 import backbone
 import torch
+import os
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
@@ -10,32 +11,40 @@ from methods.ppo_torch import Agent
 from gym import spaces
 
 class PPO_MAML(MetaTemplate):
-    def __init__(self, model_func, n_way, n_support, approx=False, test_mode=False):
+    def __init__(self, model_func, n_way, n_support, approx=False, agent_chkpt_dir = '/content/temp'):
         super(PPO_MAML, self).__init__(model_func, n_way, n_support, change_way=False)
         self.loss_fn = nn.CrossEntropyLoss()
         self.classifier = backbone.Linear_fw(self.feat_dim, n_way)
         self.classifier.bias.data.fill_(0)
         
         self.n_task = 4
-        self.task_update_num = 5 if test_mode else 3
+        self.task_update_num = 0
         self.train_lr = 0.01
         self.approx = approx
-        self.test_mode = test_mode
-        self.inner_loop_steps_list = []
+
 
         # Setup Environment
+        self.agent_chkpt_dir = os.path.join(agent_chkpt_dir, 'ppo')
+        if not os.path.isdir(self.agent_chkpt_dir):
+            os.makedirs(self.agent_chkpt_dir)
+            
         self.action_space = spaces.Discrete(5)
-        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(2,), dtype=np.float32)
-        
+        self.number_of_observations = 3
+        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.number_of_observations,), dtype=np.float32)
+        self.n_steps = 0
         self.score_history = []
         self.learn_iters = 0
         self.avg_score = 0
         self.best_score = 0
 
         # Setup Agent
-        self.agent = Agent(n_actions=self.action_space.n, batch_size=self.n_task, 
-                           alpha=0.01, n_epochs=5, 
-                           input_dims=self.observation_space.shape)
+        self.agent = Agent(n_actions=self.action_space.n,
+                           input_dims=self.observation_space.shape,
+                           chkpt_dir = agent_chkpt_dir,
+                           alpha=0.01,
+                           batch_size= 5, 
+                           n_epochs=5, 
+                           fc_dims=32 )
 
     def forward(self, x):
         out = self.feature.forward(x)
@@ -90,7 +99,8 @@ class PPO_MAML(MetaTemplate):
     
         # reset environment
         score = 0
-        observation = np.zeros(2)
+        N = 50
+        observation = np.zeros(self.number_of_observations)
     
         optimizer.zero_grad()
     
@@ -103,12 +113,12 @@ class PPO_MAML(MetaTemplate):
     
             avg_support_loss, query_loss = self.set_forward_loss(x)
     
-            observation_ = np.array([avg_support_loss.item(), query_loss.item()], dtype=np.float32)
+            observation_ = np.array([self.task_update_num, avg_support_loss.item(), query_loss.item()], dtype=np.float32)
             reward = -query_loss.item()
-    
             done = (i == len(train_loader) - 1)
             print('Done: ', done)
-            
+
+            self.n_steps += 1
             score += reward
             self.agent.remember(observation, action, prob, val, reward, done)
     
@@ -123,15 +133,16 @@ class PPO_MAML(MetaTemplate):
     
                 task_count = 0
                 all_query_loss = []
-    
+
+            if self.n_steps % N == 0: 
                 self.agent.learn()
                 self.learn_iters += 1
-    
+        
             optimizer.zero_grad()
             observation = observation_
     
             if i % print_freq == 0:
-                print(f'Epoch {epoch} | Batch {i}/{len(train_loader)} | Loss {avg_query_loss / float(i + 1):.6f}, Learn Iters {self.learn_iters}')
+                print(f'Epoch {epoch} | Batch {i}/{len(train_loader)} | Loss {avg_query_loss / float(i + 1):.6f}')
     
         self.score_history.append(score)
         self.avg_score = np.mean(self.score_history[-100:])
@@ -140,6 +151,19 @@ class PPO_MAML(MetaTemplate):
             self.best_score = self.avg_score
             self.agent.save_models()
 
+        print('Episode', epoch, 'Score %.1f' % score, 'Avg score %.1f' % self.avg_score,'Time steps', self.n_steps, 'learning_steps', self.learn_iters)
+            
+    def correct(self, x):       
+        avg_support_loss, scores = self.set_forward(x,  is_feature=False)
+        y = Variable(torch.from_numpy(np.repeat(range(self.n_way), self.n_query))).cuda()
+        
+        query_loss = self.loss_fn(scores, y)
+        y_query = np.repeat(range( self.n_way ), self.n_query )
+
+        topk_scores, topk_labels = scores.data.topk(1, 1, True, True)
+        topk_ind = topk_labels.cpu().numpy()
+        top1_correct = np.sum(topk_ind[:,0] == y_query)
+        return float(top1_correct), len(y_query), query_loss
 
     def test_loop(self, test_loader, return_std=False):
         correct = 0
@@ -147,13 +171,25 @@ class PPO_MAML(MetaTemplate):
         avg_loss = 0
         acc_all = []
 
+        # Load the best agent model
+        self.agent.load_models()
+
         iter_num = len(test_loader)
+        observation = np.zeros(self.number_of_observations)
+        
         for i, (x, _) in enumerate(tqdm(test_loader, desc='Testing', leave=False)):
             self.n_query = x.size(1) - self.n_support
             assert self.n_way == x.size(0), "MAML does not support way change"
+
+            action, prob, val = self.agent.choose_action(observation)
+            self.task_update_num = action + 1  # agent.step
+            
             correct_this, count_this, loss = self.correct(x)
             acc_all.append(correct_this / count_this * 100)
             avg_loss += loss.item()
+
+            observation_ = np.array([loss.item(), loss.item()], dtype=np.float32)
+            observation = observation_
 
         acc_all = np.asarray(acc_all)
         acc_mean = np.mean(acc_all)
